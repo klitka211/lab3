788992a613d685f186c680ac3436de85f35dadbd
klitka211 <78439685+klitka211@users.noreply.github.com>
2022-06-16 01:16:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Krzysztof Litka - Lab3/Zadanie 2. 
+          Krzysztof Litka - Lab3/Zadanie2.
         </p>
         <a
           className="App-link"
